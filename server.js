bec1f0a24157b39b21aa19eb80a567378f24d678
Akeem C <javantieclarke@gmail.com>
2021-08-02 17:17:43 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,38 +7,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
-
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
